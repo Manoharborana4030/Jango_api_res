8ff1edb1c38294c3c39c194422e55f5071a32c11
arjun <arjundarji9995@outlook.com>
2022-03-10 17:10:51 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -11,3 +11,28 @@ from django.db.models import Sum
 
 
 
+class restaurant(APIView):
+	def get(self,request):
+		obj = Restaurant.objects.all()
+		serializer = RestaurantSerializer(obj,many=True)
+		return Response(serializer.data)
+	
+	def post(self,request):
+		serializer = RestaurantSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response({'msg':'Restaurant has been added'})
+		return Response(serializer.errors)
+
+class category(APIView):
+	def get(self,request):
+		obj = Cat_Res.objects.all()
+		serializer = CatSerializer(obj,many=True)
+		return Response(serializer.data)
+	
+	def post(self,request):
+		serializer = CatSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response({'msg':'category has been added'})
+		return Response(serializer.errors)
