650cf832aa2c8edd2a22fdd002b71aa1c1faf0b7
arjun <arjundarji9995@outlook.com>
2022-03-10 16:31:39 +0530

    Update urls.py

--- Jango_api_res/urls.py
+++ Jango_api_res/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-
+from api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/',views.restaurant.as_view()),
