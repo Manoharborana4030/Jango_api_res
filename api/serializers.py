2e1ff002168b78ae1f55e71910a1b8bc00697e10
arjun <arjundarji9995@outlook.com>
2022-03-10 17:12:49 +0530

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -4,3 +4,13 @@ from rest_framework import serializers
 from .models import Cat_Res,Restaurant,Iteams
 from django.contrib.auth.models import User
 
+
+class RestaurantSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Restaurant
+		fields = '__all__' 
+
+class CatSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Cat_Res
+		fields = '__all__' 
